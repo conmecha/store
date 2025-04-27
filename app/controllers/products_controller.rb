6986eb980b20f42ac826d6f8e703953bf6c4dc53
conmecha <orcungulen79@gmail.com>
2025-04-27 18:44:13 +0300

    Initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[index show]
+  # Applying set_product to show/edit/update methods before any action
+  before_action :set_product, only: %i[show edit update destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    # Method to find product with id param
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Program will only expect written params
+    def product_params
+      params.expect(product: [ :name ])
+    end
+end
